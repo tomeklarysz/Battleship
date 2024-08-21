0bd36fa7728615d61582edc3d0bb45c0e2d2f377
tomeklarysz <ttomek.larysz@gmail.com>
2024-08-21 15:47:54 +0200

    cells that can't be assigned new ship on (named as forbidden)

--- src/gameboard.js
+++ src/gameboard.js
@@ -4,6 +4,7 @@ const gameboard = () => {
   
   let board = create2DArray(10)
   let ships = []
+  let forbiddenCells = []
   const placeShip = (size, start, end) => {
     // check if we don't go outside the board
     if (!start.every(value => value >= 0 && value < 10) || !end.every(value => value >= 0 && value < 10)) {
@@ -55,23 +56,89 @@ const gameboard = () => {
     } else throw new Error('wrong coordinates')
     
     
+    const isCellValid = (cell) => {
+      return !forbiddenCells.includes(cell)
+    }
+
     // ship placement on the board
     if (horizontal) {
       for (let i=0; i<size; i++) {
+        if (!isCellValid([start[0], start[1] + i])) {
+          alert('wrong coordinates')
+          throw new Error('wrong coordinates')
+        }
         board[start[0]][start[1] + i] = 'O'
-        ship.placements.push([start[0], start[1]+i])
+        ship.placements.push([start[0], start[1] + i])
+        forbiddenCells.push([start[0], start[1] + i])
+        
+        // add cells above and below to forbidden cells
+        if (start[0] - 1 >= 0) forbiddenCells.push([start[0] - 1, start[1] + i])
+        if (start[0] + 1 < 10) forbiddenCells.push([start[0] + 1, start[1] + i])
+      }
+
+      // add cells that are beside and diagonal to the first and last one to forbidden
+      
+      // left one
+      if (start[1] - 1 >= 0) {
+        forbiddenCells.push([start[0], start[1] - 1])
+        if (start[0] - 1 >= 0) forbiddenCells.push([start[0] - 1, start[1] - 1])
+        if (start[0] + 1 < 10) forbiddenCells.push([start[0] + 1, start[1] - 1])
+      }
+
+      // right one
+      if (end[1] + 1 < 10) {
+        forbiddenCells.push([end[0], end[1] + 1])
+        if (end[0] - 1 >= 0) forbiddenCells.push([end[0] - 1, end[1] + 1])
+        if (end[0] + 1 < 10) forbiddenCells.push([end[0] + 1, end[1] + 1])
       }
     }
+
     if (vertical) {
       const col = start[1]
       let i=0
       while (i < size) {
+        if (!isCellValid([start[0] + i, col])) {
+          alert('wrong coordinates')
+          throw new Error('wrong coordinates')
+        }
         board[start[0] + i][col] = 'O'
         ship.placements.push([start[0] + i, col])
+        forbiddenCells.push([start[0] + i, col])
+
+        // add cells to forbidden cells that are next to current cell
+        if (col - 1 >= 0) forbiddenCells.push([start[0] + i, col - 1])
+        if (col + 1 < 10) forbiddenCells.push([start[0] + i, col + 1])
+
         i++
       }
+
+      // add cells to forbidden that are above and diagonal to the top cell
+      if (start[0] - 1 >= 0) {
+        forbiddenCells.push([start[0] - 1, col])
+        if (col - 1 >= 0) forbiddenCells.push([start[0] - 1, col - 1])
+        if (col + 1 < 10) forbiddenCells.push([start[0] - 1, col + 1])
+      }
+
+      // add cells to forbidden that are below and diagonal to the bottom cell
+      if (end[0] + 1 < 10) {
+        forbiddenCells.push([end[0] + 1, col])
+        if (col - 1 >= 0) forbiddenCells.push([end[0] + 1, col - 1])
+        if (col + 1 < 10) forbiddenCells.push([end[0] + 1, col + 1])
+      }
     }
+    removeDuplicates(forbiddenCells)
     return ship
+  }
+
+  const removeDuplicates = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        if (JSON.stringify(array[i]) === JSON.stringify(array[j])) {
+          array.splice(j, 1);
+          j--;
+        }
+      }
+    }
   }
 
   const receiveAttack = (ships, row, col) => {
@@ -108,7 +175,7 @@ const gameboard = () => {
     }
   }
 
-  return { board, placeShip, receiveAttack, ships }
+  return { board, placeShip, receiveAttack, ships, forbiddenCells }
 }
 
 
