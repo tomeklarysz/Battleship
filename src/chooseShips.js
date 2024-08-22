cbebd93389d05e5418b3ae0b132df160184469a2
tomeklarysz <ttomek.larysz@gmail.com>
2024-08-22 16:21:11 +0200

    methods after clicking last cell of ship

--- src/chooseShips.js
+++ src/chooseShips.js
@@ -1,43 +1,46 @@
 import { initialBoardRender, renderCell, readCords } from "./render"
 import { removeGrids, displayGrids } from "./dom"
-const create2DArray = require('./gameboard')
+const gameboard = require('./gameboard')
 
+let ships = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4]
 const chooseShipsHandler = (players) => {
   
   // clear user board
-  players.user.gameboard.board = create2DArray(10)
+  players.user.gameboard = gameboard()
   console.log('clicked')
   removeGrids()
   displayGrids()
 
   document.querySelectorAll('.user').forEach(cell => {
-    cell.style.cursor = 'pointer'
+    cell.classList.toggle('point')
   })
 
-  let ships = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4]
-  let forbiddenCells = players.user.gameboard.forbiddenCells
   
+  document.addEventListener('click', pickOnlyFirstCell)
   document.addEventListener('click', (e) => {
-    pickOnlyFirstCell(e, ships)    
+    pickLastCell(e, players)
   })
-      
-  // initialBoardRender(players)
 }
 
-const pickOnlyFirstCell = (e, ships) => {
+const pickOnlyFirstCell = (e) => {
   if (e.target.matches('.user')) {
-    const cords = readCords(e.target)
-    e.target.classList.add('potential')
-    e.target.textContent = 1
-    document.removeEventListener('click', pickOnlyFirstCell)
-    calculatePotential(ships)
+    if (!e.target.matches('.forbidden') &&
+       !e.target.matches('.ship')) {
+      e.target.classList.add('potential')
+      e.target.textContent = 1
+      document.removeEventListener('click', pickOnlyFirstCell)
+      document.querySelectorAll('.user').forEach(cell => {
+        cell.classList.toggle('point')
+      })
+      calculatePotential(ships)
+    }
   }
 }
 
 const calculatePotential = (ships) => {
   const potential = document.querySelector('.potential')
-  const num = ships.pop()
-  console.log(num)
+  // potential.classList.toggle('point')
+  const num = ships.slice(-1).pop()
   const cords = readCords(potential)
   const row = cords[0]
   const col = cords[1]
@@ -53,7 +56,7 @@ const showHoverInfo = (row, col, num) => {
 
   document.addEventListener('mouseover', e => {
     if (e.target.matches('.toBeShip')) {
-      e.target.style.backgroundColor = 'lightgray'
+      e.target.classList.add('show')
       e.target.style.color = 'black'
     }
   })
@@ -93,7 +96,60 @@ const addCellStyle = (current, index, num) => {
   current.textContent = `${index+1}`
   if (index == num - 1) {
     current.classList.add('last')
+    current.classList.toggle('point')
   }
+}
+
+const pickLastCell = (e, players) => {
+  if (e.target.matches('.last')) {
+    if (ships.length === 0) {
+      alert('all ships chosen!')
+    }
+    const size = ships.pop()
+    const firstCords = readCords(document.querySelector('.potential'))
+    const lastCords = readCords(e.target)
+    console.log(players)
+    players.user.gameboard.placeShip(size, firstCords, lastCords)
+    removeCellStyle()
+    initialBoardRender(players)
+    addForbiddenStyle(players)
+    document.addEventListener('click', pickOnlyFirstCell)
+    document.querySelectorAll('.user').forEach(cell => {
+      if (!cell.classList.contains('ship') &&
+        !cell.classList.contains('forbidden')) {
+        cell.classList.toggle('point')
+      }
+    })
+  }
+}
+
+const removeCellStyle = () => {
+  document.querySelectorAll('.toBeShip').forEach(item => {
+    item.textContent = ''
+    if (item.classList.contains('last')) {
+      item.classList.remove('last')
+    }
+    if (item.classList.contains('show')) {
+      item.classList.remove('show')
+    }
+    item.classList.remove('toBeShip')
+    item.classList.remove('point')
+  })
+  document.querySelector('.potential').textContent = ''
+  document.querySelector('.potential').classList.remove('potential')
+}
+
+const addForbiddenStyle = (players) => {
+  const forbiddenCells = players.user.gameboard.forbiddenCells
+  forbiddenCells.forEach(item => {
+    const row = item[0]
+    const col = item[1]
+    const current = document.getElementById(`player-${row}-${col}`)
+    if (!(current.classList.contains('ship'))) {
+      // current.classList.toggle('point')
+      current.classList.add('forbidden')
+    }
+  })
 }
 
 export const randomShips = (players) => {
